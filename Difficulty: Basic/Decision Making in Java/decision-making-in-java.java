96084324688d6cf43e172497f3bd1124c543b77f
srinivas280205 <srinivas280205@gmail.com>
2025-07-23 19:56:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Decision Making in Java/decision-making-in-java.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+class Solution {
+    public static String compareNM(int n, int m) {
+        // code here
+      
+        if(n<m)
+               return "lesser";
+            
+        
+        else if(n==m)
+           return "equal";
+        
+        else
+               return "greater";
+     } 
+}
