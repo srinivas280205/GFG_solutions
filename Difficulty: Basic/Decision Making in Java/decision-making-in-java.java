f03c0120a3d51199fae8fcff4f31821b687647d6
srinivas280205 <srinivas280205@gmail.com>
2025-07-23 19:58:29 +0530

    decision-making-in-java.java

--- Difficulty: Basic/Decision Making in Java/decision-making-in-java.java
+++ Difficulty: Basic/Decision Making in Java/decision-making-in-java.java
@@ -1,16 +1,11 @@
 // User function Template for Java
 class Solution {
     public static String compareNM(int n, int m) {
-        // code here
-      
         if(n<m)
-               return "lesser";
-            
-        
+            return "lesser";
         else if(n==m)
-           return "equal";
-        
+            return "equal";   
         else
-               return "greater";
+            return "greater";
      } 
 }
